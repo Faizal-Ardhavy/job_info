1fdb0e84022d423e524da01b0a3a5833844ec5e3
Naufal Anbial Falah <naufalanbial@gmail.com>
2024-02-10 17:48:57 +0700

    init

---
+++ src/components/navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Navbar() {
+	return (
+		<header className='position-fixed top-0 left-0 px-16 py-5 bg-slate-400 text-left flex justify-between'>
+			<div>
+				Logo
+			</div>
+			<div className='flex gap-3'>
+				<div>
+					Link 1
+				</div>
+				<div>
+					Link 1
+				</div>
+			</div>
+		</header>
+	)
+}
